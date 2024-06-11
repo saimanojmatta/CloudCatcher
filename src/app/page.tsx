1a6892b8c4ad2e810a4b9ea3a55d5ee9999482c1
manoj <142380042+saimanojmatta@users.noreply.github.com>
2024-06-11 07:23:02 +0530

    new branch is created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,18 @@ import TimeLocation from "@/components/TimeLocation"
 import WeatherStats from "@/components/WeatherStats"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 type Props = {}
 const Page = (props: Props) => {
   const[data,setData]=useState({})
-  const[city,setCity]=useState('')
+  const[city,setCity]=useState('mumbai')
   const[error,setError]=useState('')
 
+
   const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
- 
+  
   
   const handleSubmit=async(e:React.FormEvent<HTMLFormElement>)=>{
     e.preventDefault()
@@ -31,6 +32,8 @@ const Page = (props: Props) => {
       setData({})
     }
   }
+
+ 
    
  
   return (
@@ -38,10 +41,10 @@ const Page = (props: Props) => {
     <div className="p-28">
       {/*  input form submission */}
       <form onSubmit={handleSubmit} className="flex items-center justify-center gap-4">
-      <Input type="text"placeholder="enter city name or zip code" onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setCity(e.target.value)} />
+      <Input  type="text"placeholder="enter city name or zip code" onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setCity(e.target.value)} />
       <Button type="submit">Search</Button>
       </form>
-
+      
       {Object.keys(data).length===0 && error===""?
       <div className="text-center h-screen mt-[5rem]">
         <h2 className="text-3xl font-semibold mb-4"><span className="text-primary">Welcome</span> to Cloud Catcher</h2>
